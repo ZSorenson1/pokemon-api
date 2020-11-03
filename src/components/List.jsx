368a7f57a1c80b18b73373fbb34446bc672f93a6
Zachary Sorenson <zacharyjsorenson@gmail.com>
2020-11-03 15:54:23 -0500

    added code for axios assignment below original fetch

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 const List = () => {
     const [pokemon, setPokemon] = useState([]);
     
     const clickHandler = () => {
-        fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=805')
-        .then(response => response.json())
-        .then(response => setPokemon(response.results))
+        // fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=805')
+        // .then(response => response.json())
+        // .then(response => setPokemon(response.results))
+        
+        axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=805')
+        .then(response=> setPokemon(response.data.results))
+
     }
 
     return (
